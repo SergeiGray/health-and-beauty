83db408cc6946b825c0b8390c2d20588b3fcab77
SergeiGray <borodat_for@mail.ru>
2019-09-21 20:23:13 +0300

    Блок partner

--- source/js/script.js
+++ source/js/script.js
@@ -2,5 +2,28 @@
 
 (function () {
 
+  var hangFlexslider = function () {
+    $(document).ready(function() {
+      $('.team__slider').flexslider({
+        selector: ".team__slider_container > li",
+        animation: "slide",
+        slideshow: false,
+        smoothHeight: true,
+        keyboard: true,
+        prevText: "",
+        nextText: "",
+        itemWidth: 1875,
+        itemMargin: 0,
+        minItems: 1,
+        maxItems: 1,
+        controlNav: false,
+        directionNav: true,
+        touch: true,
+        move: 1
+      });
+    });
+  };
+
+  hangFlexslider();
 
 }());
